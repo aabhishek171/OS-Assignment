091785286e5c4bd51cdc497c0fc6dce00f44ac87
aabhishek171 <42514675+aabhishek171@users.noreply.github.com>
2019-04-09 01:58:51 +0530

    Add files via upload

---
+++ code2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<sys/types.h>
+#include<fcntl.h>
+#include<sys/stat.h>
+#include<unistd.h>
+int main()
+{
+	int total_process;
+	printf("Enter the number of process you want to enter = ");
+	scanf("%d",total_process);
+	
+}
