0df77605c4c8ad1cb24436a1cd4aa17c96997adf
aabhishek171 <42514675+aabhishek171@users.noreply.github.com>
2019-04-12 23:43:29 +0530

    Update code2.c

--- code2.c
+++ code2.c
@@ -5,14 +5,19 @@
 #include<unistd.h>
 int main()
 {
-	int n;			//total_process
-	int burst_time[5][3];
+	int n;		//total_process
+	int i;
+	int burst_time[5];
+	int arrival_time[5];
 	printf("Enter the number of process you want to enter = ");
-	scanf("%d",n);
-	
-	printf("Enter burst time for each process = ")
-	for(int i=0;i<n;i++)
+	scanf("%d",&n);
+
+	for(i=0;i<n;i++)
 	{
-		scanf("%d",burst_time[i][0]);
+		printf("Enter Burst Time for P%d Process = ",i+1);
+		scanf("%d",&burst_time[i]);
+		
+		printf("Enter Arrival Time for P%d Process = ",i+1);
+		scanf("%d",&arrival_time[i]);
 	}
 }
